08abe031ee68f64baac7f1fce10cb281c040dbe6
Anton Istomin <istomanton@gmail.com>
2023-07-26 15:35:55 +0400

    Smaple generation schem

--- src/schem.py
+++ src/schem.py
@@ -1,12 +1,10 @@
 import mcschematic
 
-from util import is_bit_set
-
-
 AIR = 'minecraft:air'
 TORCH = 'minecraft:redstone_wall_torch[facing=north,lit=false]'
 FRAMES = 26
 SCREEN_SIZE = 32
+PIXEL_ADDRESSES = SCREEN_SIZE * SCREEN_SIZE * FRAMES
 
 class DisplayRomBuilder:
     rom: mcschematic.MCSchematic
@@ -16,24 +14,26 @@ class DisplayRomBuilder:
         self.rom = mcschematic.MCSchematic('schems/template_rom.schem')
 
     def save(self, filename: str) -> None:
-        self.rom.save('../schems', filename, mcschematic.Version.JE_1_18_2)
+        self.rom.save('schems', filename, mcschematic.Version.JE_1_18_2)
 
-    def write_byte(self, byte: int, address: int) -> None:
-        if address > 31:
-            raise Exception(f'Byte address is {address} but only 0-31 byte addresses are supported')
+    def write_pixel(self, frame: int, x: int, y: int, value: bool) -> None:
+        if frame >= FRAMES:
+            raise Exception(f'Frame is {frame} but only 0-{FRAMES - 1} frames are supported')
 
-        z = -7
+        if x >= SCREEN_SIZE or y >= SCREEN_SIZE:
+            raise Exception(f'(x,y) is ({x},{y}) but only 0-{SCREEN_SIZE} coordinates are supported')
 
-        if address > 0xF:
-            z = -1
+        # Z is frames axis
+        first_frame_z = 2
+        last_frame_z = 52
+        rom_z = frame * 2 + first_frame_z
+        rom_x = -x * 2 - 2
+        rom_y = y * 4 + 1
 
-        x = (address & 0b1111) * 2
-        y_offset = address % 2
-
-        for i in range(8):
-            if is_bit_set(byte, i):
-                y = (i * 2 - 14) + y_offset
-                self.rom.setBlock((x, y, z), AIR)
+        if value:
+            self.rom.setBlock((rom_x, rom_y, rom_z), TORCH)
+        else:
+            self.rom.setBlock((rom_x, rom_y, rom_z), AIR)
 
     def inspect(self):
         size = 200
@@ -70,24 +70,17 @@ class DisplayRomBuilder:
         print('min_y, max_y:', min_y, max_y)
         print('min_z, max_z:', min_z, max_z)
 
-        # for x in range(31):
-        #     for y in reversed(range(-15, 2)):
-        #         z = -7
-        #         block_data = self.rom.getBlockDataAt((x, y, z))
-        #         print(x, y, z, block_data)
-
 
 def generate_schem(filepath: str):
-    # file_handle = open(filepath, 'r')
-    # file = file_handle.read()
-    # bytes_str = file.strip().replace('\n', ' ').split(' ')
-    # bytes_int = list(map(lambda x: int(x, 16), bytes_str))
     builder = DisplayRomBuilder()
-    builder.inspect()
-    # for i in range(len(bytes_int)):
-    #     builder.write_byte(bytes_int[i], i)
-    #
-    # builder.save('modified')
+    # builder.inspect()
+    for frame in range(FRAMES):
+        for x in range(SCREEN_SIZE):
+            for y in range(SCREEN_SIZE):
+                if x > frame or y > frame:
+                    builder.write_pixel(frame=frame, x=x, y=y, value=False)
+
+    builder.save('modified')
 
 
 if __name__ == '__main__':
--- src/util.py
+++
@@ -1,2 +0,0 @@
-def is_bit_set(byte: int, bit_address: int) -> bool:
-    return (byte >> bit_address) % 2 == 1
